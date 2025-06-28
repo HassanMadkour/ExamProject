148bbe5c7375c78269451a340b8556605ed448eb
Amin Elsorogy <aminelsorogy@gmail.com>
2025-06-28 15:25:47 +0300

    add merge with adminfeature

--- ExamProject/Program.cs
+++ ExamProject/Program.cs
@@ -29,7 +29,7 @@ namespace ExamProject {
                 )
                 .AddEntityFrameworkStores<ExamDbContext>().AddDefaultTokenProviders();
             builder.Services.AddDbContext<ExamDbContext>(
-                Options => Options.UseSqlServer(builder.Configuration.GetConnectionString("ExamDbConnection"))
+                Options => Options.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("ExamDbConnection"))
                 );
 
             // Add services to the container.
